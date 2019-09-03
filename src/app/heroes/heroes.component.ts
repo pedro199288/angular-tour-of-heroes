a9dd4202b04def4975ab7cc5462d7f6a02871b27
Pedro Monteagudo <pedromonteagudo_@hotmail.com>
2019-09-03 17:12:19 +0200

    starting http section, routing finished

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -19,10 +19,6 @@ export class HeroesComponent implements OnInit {
   ngOnInit() {
     this.getHeroes();
   }
-  
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
 
   getHeroes(): void {
     this.heroService.getHeroes()
